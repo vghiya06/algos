236541921de24f6a6031416d9f180be7d0429cac
Vaibhav <vghiya06@iitkgp.ac.in>
2017-02-25 00:30:37 +0530

    nonincreasing

---
+++ insertion_decreasing.java
@@ -0,0 +1,28 @@
+public class insertion_decreasing{
+public static void main (String[] args){
+
+int arr[]={5,2,4,6,1,3};
+int temp,i,j,key;
+int n=arr.length;
+for (i=1;i<n;i++)
+{
+
+key=arr[i];
+
+j=i-1;
+
+while(j>=0 && arr[j]<key){
+arr[j+1]=arr[j];
+j--;
+}
+arr[j+1]=key;
+
+}
+for (i=0;i<n;i++){
+System.out.print(arr[i]+" ");
+
+System.out.println();
+
+}
+}
+}
